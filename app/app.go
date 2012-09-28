869f02d57724d453d3ef00b7c4960c5d202277be
JLarky <jlarky@gmail.com>
2012-09-28 13:12:12 +0400

    apply 'go fmt'

--- app/app.go
+++ app/app.go
@@ -1,146 +1,156 @@
 package app
 
 import (
-    "fmt"
-    "net/http"
-    "appengine"
-    "appengine/datastore"
-    "appengine/urlfetch"
-    "log"
-    "io/ioutil"
-    "bytes"
-    "net/url"
-    "strings"
-    "text/template"
-    "time"
-    "app/cp1251_utf8"
+	"app/cp1251_utf8"
+	"appengine"
+	"appengine/datastore"
+	"appengine/urlfetch"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"text/template"
+	"time"
 )
 
 type FreeNumbers struct {
-    Date		time.Time
-    Gynaecologists	int // акушер-гинекол.
-    Venereologist	int // дерма-венеролог
-    Cardiologist	int // кардиолог
-    Neurologist 	int // невропатолог
-    Otolaryngologist	int // отоларинголог
-    Ophthalmologist	int // офтальмолог
-    Proctologist	int // проктолог
-    Rheumatologist	int // ревматолог
-    Physician		int // терапевт участ.
-    Urologist		int // уролог
-    Surgeon		int // хирург
-    Endocrinologist	int // эндокринолог
+	Date             time.Time
+	Gynaecologists   int // акушер-гинекол.
+	Venereologist    int // дерма-венеролог
+	Cardiologist     int // кардиолог
+	Neurologist      int // невропатолог
+	Otolaryngologist int // отоларинголог
+	Ophthalmologist  int // офтальмолог
+	Proctologist     int // проктолог
+	Rheumatologist   int // ревматолог
+	Physician        int // терапевт участ.
+	Urologist        int // уролог
+	Surgeon          int // хирург
+	Endocrinologist  int // эндокринолог
 }
 
 func init() {
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/cron", update)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/cron", update)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    data := get_olddata(w, r)
-    if err := IndexTemplate.Execute(w, data); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	data := get_olddata(w, r)
+	if err := IndexTemplate.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func update(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    a := FreeNumbers{Date: time.Now()}
+	c := appengine.NewContext(r)
+	a := FreeNumbers{Date: time.Now()}
 
-    var s = post(r, "http://nikmed.spb.ru/cgi-bin/tcgi1.exe", "COMMAND=2")
-    var prefix = "<span style=\"text-align:center;font-size:large;font-family:arial\">"
-    var prefix_len = len(prefix)
-    var suffix_lef = len("</span>")
-    for _, i := range strings.Split(s, "\n") {
-        if strings.HasPrefix(i, prefix) {
-	    var s = i[prefix_len:len(i)-suffix_lef-1]
-	    s = strings.TrimSpace(strings.ToLower(s))
-	    fields := strings.Split(s, " ")
-	    f_len := len(fields)
-	    name := strings.Join(fields[0:f_len-2], " ")
-	    count := fields[f_len-1]
-	    var count_int int
-	    if _, e := fmt.Sscan(count, &count_int); e != nil {
-		count_int = 0
-	    }
-	    switch name {
-		case "акушер-гинекол.":	a.Gynaecologists = count_int
-		case "дерма-венеролог":	a.Venereologist = count_int
-		case "кардиолог":	a.Cardiologist = count_int
-		case "невропатолог":	a.Neurologist = count_int
-		case "отоларинголог":	a.Otolaryngologist = count_int
-		case "офтальмолог":	a.Ophthalmologist = count_int
-		case "проктолог":	a.Proctologist = count_int
-		case "ревматолог":	a.Rheumatologist = count_int
-		case "терапевт участ.":	a.Physician = count_int
-		case "уролог":		a.Urologist = count_int
-		case "хирург":		a.Surgeon = count_int
-		case "эндокринолог":	a.Endocrinologist = count_int
-	    }
-            fmt.Fprint(w, name, " -> ", count_int, "\n")
+	var s = post(r, "http://nikmed.spb.ru/cgi-bin/tcgi1.exe", "COMMAND=2")
+	var prefix = "<span style=\"text-align:center;font-size:large;font-family:arial\">"
+	var prefix_len = len(prefix)
+	var suffix_lef = len("</span>")
+	for _, i := range strings.Split(s, "\n") {
+		if strings.HasPrefix(i, prefix) {
+			var s = i[prefix_len : len(i)-suffix_lef-1]
+			s = strings.TrimSpace(strings.ToLower(s))
+			fields := strings.Split(s, " ")
+			f_len := len(fields)
+			name := strings.Join(fields[0:f_len-2], " ")
+			count := fields[f_len-1]
+			var count_int int
+			if _, e := fmt.Sscan(count, &count_int); e != nil {
+				count_int = 0
+			}
+			switch name {
+			case "акушер-гинекол.":
+				a.Gynaecologists = count_int
+			case "дерма-венеролог":
+				a.Venereologist = count_int
+			case "кардиолог":
+				a.Cardiologist = count_int
+			case "невропатолог":
+				a.Neurologist = count_int
+			case "отоларинголог":
+				a.Otolaryngologist = count_int
+			case "офтальмолог":
+				a.Ophthalmologist = count_int
+			case "проктолог":
+				a.Proctologist = count_int
+			case "ревматолог":
+				a.Rheumatologist = count_int
+			case "терапевт участ.":
+				a.Physician = count_int
+			case "уролог":
+				a.Urologist = count_int
+			case "хирург":
+				a.Surgeon = count_int
+			case "эндокринолог":
+				a.Endocrinologist = count_int
+			}
+			fmt.Fprint(w, name, " -> ", count_int, "\n")
+		}
 	}
-    }
-    _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "FreeNumbers", nil), &a)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "FreeNumbers", nil), &a)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
-
 func get_olddata(w http.ResponseWriter, r *http.Request) string {
-    c := appengine.NewContext(r)
-    q := datastore.NewQuery("FreeNumbers").
-        Order("-Date").
-        Limit(4032)
-    nums := make([]FreeNumbers, 0, 10)
-    if _, err := q.GetAll(c, &nums); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-    data1 := bytes.NewBufferString("");
-    data2 := bytes.NewBufferString("");
-    for _, n := range nums {
-         fmt.Fprintf(data1, "\t  [new Date(%d), \t%d, %d, %d, %d, %d, %d],\n",
-               n.Date.Unix()*1000, n.Gynaecologists, n.Venereologist, n.Cardiologist,
-                            n.Neurologist, n.Otolaryngologist, n.Ophthalmologist)
-         fmt.Fprintf(data2, "\t  [new Date(%d), \t%d, %d, %d, %d, %d, %d],\n",
-               n.Date.Unix()*1000, n.Proctologist, n.Rheumatologist, n.Physician,
-                            n.Urologist, n.Surgeon, n.Endocrinologist)
-    }
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("FreeNumbers").
+		Order("-Date").
+		Limit(4032)
+	nums := make([]FreeNumbers, 0, 10)
+	if _, err := q.GetAll(c, &nums); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	data1 := bytes.NewBufferString("")
+	data2 := bytes.NewBufferString("")
+	for _, n := range nums {
+		fmt.Fprintf(data1, "\t  [new Date(%d), \t%d, %d, %d, %d, %d, %d],\n",
+			n.Date.Unix()*1000, n.Gynaecologists, n.Venereologist, n.Cardiologist,
+			n.Neurologist, n.Otolaryngologist, n.Ophthalmologist)
+		fmt.Fprintf(data2, "\t  [new Date(%d), \t%d, %d, %d, %d, %d, %d],\n",
+			n.Date.Unix()*1000, n.Proctologist, n.Rheumatologist, n.Physician,
+			n.Urologist, n.Surgeon, n.Endocrinologist)
+	}
 
-    data := bytes.NewBufferString("");
-    fmt.Fprintf(data, "data.addRows([\n%s\n]);\ndata2.addRows([\n%s\n]);", data1, data2)
-    return data.String()
+	data := bytes.NewBufferString("")
+	fmt.Fprintf(data, "data.addRows([\n%s\n]);\ndata2.addRows([\n%s\n]);", data1, data2)
+	return data.String()
 }
 
 func post(r *http.Request, uri, postvars string) string {
-    c:=appengine.NewContext(r)
-    deadline, _ := time.ParseDuration("5s")
-    t := urlfetch.Transport{Context:c, Deadline: deadline}
-    client := http.Client{Transport: &t}
-    // resp, err := client.Get("http://punklan.net/cp1251")
-    var params, _ = url.ParseQuery(postvars)
-    resp, err := client.PostForm(uri, params)
-    if err != nil {
-        log.Print("err %s", err.Error())
-    }
-    b,err:=ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Print("err %s", err.Error())
-    }
-    buffer := bytes.NewBufferString("");
-    for _, char := range b {
-         var ch = cp1251_utf8.Utf(char)
-         fmt.Fprintf(buffer, "%c", ch)
-    }
-    return string(buffer.Bytes())
+	c := appengine.NewContext(r)
+	deadline, _ := time.ParseDuration("5s")
+	t := urlfetch.Transport{Context: c, Deadline: deadline}
+	client := http.Client{Transport: &t}
+	// resp, err := client.Get("http://punklan.net/cp1251")
+	var params, _ = url.ParseQuery(postvars)
+	resp, err := client.PostForm(uri, params)
+	if err != nil {
+		log.Print("err %s", err.Error())
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print("err %s", err.Error())
+	}
+	buffer := bytes.NewBufferString("")
+	for _, char := range b {
+		var ch = cp1251_utf8.Utf(char)
+		fmt.Fprintf(buffer, "%c", ch)
+	}
+	return string(buffer.Bytes())
 }
 
 var IndexTemplate = template.Must(template.New("book").Parse(IndexHTML))
 
-const IndexHTML =
-`<!DOCTYPE html>
+const IndexHTML = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8" />
